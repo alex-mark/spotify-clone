df84127cd451ea7b2e62f7bff90834ddeb7638b4
Alex Markin <alexndr86@gmail.com>
2020-10-08 12:14:56 +0300

    Connect react to spotify

--- src/App.js
+++ src/App.js
@@ -1,18 +1,27 @@
 import React, { useEffect, useState } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
 import "./App.css";
 import Login from "./components/Login";
 import { getTokenFromUrl } from "./spotify";
+
+const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
 
   useEffect(() => {
     const hash = getTokenFromUrl();
-    // window.location.hash = "";
+    window.location.hash = "";
     const _token = hash.access_token;
 
     if (_token) {
       setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then((user) => {
+        console.log("User", user);
+      });
     }
 
     console.log("I have a token >>>", _token);

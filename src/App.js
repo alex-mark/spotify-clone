d8ded666a86ddcd7b481956a61238d6491e6d88b
Alex Markin <alexndr86@gmail.com>
2020-10-08 13:07:24 +0300

    Add dispatch set_user

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import "./App.css";
 import Login from "./components/Login";
 import Player from "./components/Player";
 import { getTokenFromUrl } from "./spotify";
+import { useStateValue } from "./StateProvider";
 
 const spotify = new SpotifyWebApi();
 
 function App() {
   const [token, setToken] = useState(null);
+  const [{ user }, dispatch] = useStateValue();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -21,12 +23,15 @@ function App() {
       spotify.setAccessToken(_token);
 
       spotify.getMe().then((user) => {
-        console.log("User", user);
+        dispatch({
+          type: "SET_USER",
+          user: user,
+        });
       });
     }
-
-    console.log("I have a token >>>", _token);
   }, []);
+
+  console.log("user from state", user);
 
   return <div className="app">{token ? <Player /> : <Login />}</div>;
 }

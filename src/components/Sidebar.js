63be525f281355cd169eb1ff7853ee088cd922d2
Alex Markin <alexndr86@gmail.com>
2020-10-08 19:00:00 +0300

    Style sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,9 @@
 import React from "react";
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 import "./Sidebar.css";
+import SidebarOption from "./SidebarOption";
 
 function Sidebar() {
   return (
@@ -9,6 +13,16 @@ function Sidebar() {
         src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
         alt="spotify logo"
       />
+      <SidebarOption title="Home" Icon={HomeIcon} />
+      <SidebarOption title="Search" Icon={SearchIcon} />
+      <SidebarOption title="Your Library" Icon={LibraryMusicIcon} />
+
+      <br />
+      <strong className="sidebar__title">PLAYLISTS</strong>
+      <hr />
+      <SidebarOption title="Hip hop" />
+      <SidebarOption title="Rock" />
+      <SidebarOption title="RnB" />
     </div>
   );
 }
